26dfdb5086833cfc3e18494b0f8371fde526357b
canalog <alicecha719@naver.com>
2021-03-24 21:41:58 +0900

    test

--- src/MariaDBConnect.java
+++ src/MariaDBConnect.java
@@ -1,71 +1,48 @@
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-// asdf
 public class MariaDBConnect {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		final String driver = "org.mariadb.jdbc.Driver";
 		final String DB_IP = "localhost";
 		final String DB_PORT = "3306";
-		final String DB_NAME = "corona";
+		final String DB_NAME = "nhis";
 		final String DB_URL = 
 				"jdbc:mariadb://" + DB_IP + ":" + DB_PORT + "/" + DB_NAME;
-
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-
+		
 		try {
-			Class.forName(driver);
-			conn = DriverManager.getConnection(DB_URL, "root", "password");
-			if (conn != null) {
-				System.out.println("DB connected!");
-			}
-
-		} catch (ClassNotFoundException e) {
-			System.out.println("Driver loading failed");
+			Class.forName("org.mariadb.jdbc.Driver");
+		}
+		catch(ClassNotFoundException e){
 			e.printStackTrace();
-		} catch (SQLException e) {
-			System.out.println("DB connection failed");
+		}
+		
+		Connection conn = null;	
+		Statement stmt = null;
+		
+		try {
+			//connect DB
+			conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306","root","password");
+			System.out.println("Connection Success!");
+			
+			//create sql statements
+			stmt = conn.createStatement();
+			stmt.executeUpdate("use nhis");
+			
+			//resultsets = get the result of sql queries
+			ResultSet rs = stmt.executeQuery("select * from nhis_10000");
+			while(rs.next()) {
+				System.out.println(rs.getString(1)+"\t"+rs.getInt(2)+"\t"+rs.getString(3)+"\t"
+			+rs.getString(4)+"\t"+rs.getInt(5));
+			}
+		}
+		catch(SQLException e) {
 			e.printStackTrace();
 		}
 
-		try {
-			String sql = "select * from estimator";
-
-			pstmt = conn.prepareStatement(sql);
-
-			rs = pstmt.executeQuery();
-			String userId = null;
-			String password = null;
-			String name = null;
-			while (rs.next()) {
-				userId = rs.getString(1);
-				password = rs.getString(2);
-				name = rs.getString(3);
-			}
-
-			System.out.println(userId);
-			System.out.println(password);
-			System.out.println(name);
-
-		} catch (SQLException e) {
-			System.out.println("error: " + e);
-		} finally {
-			try {
-				if (rs != null) {
-					rs.close();
-				}
-				if (pstmt != null) {
-					pstmt.close();
-				}
-
-				if (conn != null && !conn.isClosed()) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
 	}
 }
