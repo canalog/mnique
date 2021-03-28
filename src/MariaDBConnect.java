80a640cf8954f744376871680559bd4ca783b450
canalog <alicecha719@naver.com>
2021-03-28 21:27:16 +0900

    test

--- src/MariaDBConnect.java
+++ src/MariaDBConnect.java
@@ -1,10 +1,35 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MariaDBConnect {
+	
+	public static Map<String, Object> get_table(Statement stmt, String table) throws SQLException {
+		try {
+			Map<String, Object> tableMap = new HashMap<String, Object>();	
+			//resultsets = get the result of sql queries
+			ResultSet rs = stmt.executeQuery("select * from " + table);
+			ResultSetMetaData rsmd = rs.getMetaData();
+			int columnCnt = rsmd.getColumnCount();
+			while(rs.next()) {
+				for (int i = 1; i <= columnCnt; i++) {
+					tableMap.put(rsmd.getColumnName(i), rs.getString(rsmd.getColumnName(i)));
+				}
+	//			System.out.println(rs.getString(1)+"\t"+rs.getInt(2)+"\t"+rs.getString(3)+"\t"
+	//		+rs.getString(4)+"\t"+rs.getInt(5));
+			}
+			return tableMap;
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		final String driver = "org.mariadb.jdbc.Driver";
@@ -33,12 +58,23 @@ public class MariaDBConnect {
 			stmt = conn.createStatement();
 			stmt.executeUpdate("use nhis");
 			
-			//resultsets = get the result of sql queries
-			ResultSet rs = stmt.executeQuery("select * from nhis_10000");
-			while(rs.next()) {
-				System.out.println(rs.getString(1)+"\t"+rs.getInt(2)+"\t"+rs.getString(3)+"\t"
-			+rs.getString(4)+"\t"+rs.getInt(5));
-			}
+			Map<String, Object> nhis_10000 = get_table(stmt, "nhis_10000");
+			Map<String, Object> r1 = get_table(stmt, "r1");
+			r1.forEach((key, value) -> System.out.println(key + ":" + value));
+//			Map<String, Object> original = new HashMap<String, Object>();
+//			
+//			//resultsets = get the result of sql queries
+//			ResultSet rs = stmt.executeQuery("select * from nhis_10000");
+//			ResultSetMetaData rsmd = rs.getMetaData();
+//			int columnCnt = rsmd.getColumnCount();
+//			while(rs.next()) {
+//				for (int i = 1; i <= columnCnt; i++) {
+//					original.put(rsmd.getColumnName(i), rs.getString(rsmd.getColumnName(i)));
+//				}
+////				System.out.println(rs.getString(1)+"\t"+rs.getInt(2)+"\t"+rs.getString(3)+"\t"
+////			+rs.getString(4)+"\t"+rs.getInt(5));
+//			}
+//			original.forEach((key, value) -> System.out.println(key + ":" + value));
 		}
 		catch(SQLException e) {
 			e.printStackTrace();
