ac7681535d07edc2938f32702ce2c60c57f341b8
canalog <alicecha719@naver.com>
2021-03-28 21:30:32 +0900

    0328 get db table

--- src/MariaDBConnect.java
+++ src/MariaDBConnect.java
@@ -1,84 +1,98 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
+
 
 public class MariaDBConnect {
-	
-	public static Map<String, Object> get_table(Statement stmt, String table) throws SQLException {
-		try {
-			Map<String, Object> tableMap = new HashMap<String, Object>();	
-			//resultsets = get the result of sql queries
-			ResultSet rs = stmt.executeQuery("select * from " + table);
-			ResultSetMetaData rsmd = rs.getMetaData();
-			int columnCnt = rsmd.getColumnCount();
-			while(rs.next()) {
-				for (int i = 1; i <= columnCnt; i++) {
-					tableMap.put(rsmd.getColumnName(i), rs.getString(rsmd.getColumnName(i)));
-				}
-	//			System.out.println(rs.getString(1)+"\t"+rs.getInt(2)+"\t"+rs.getString(3)+"\t"
-	//		+rs.getString(4)+"\t"+rs.getInt(5));
-			}
-			return tableMap;
-		}
-		catch(SQLException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+	static Connection conn = null;	
+	static Statement stmt = null;
+	static ResultSet rs = null;
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		final String driver = "org.mariadb.jdbc.Driver";
-		final String DB_IP = "localhost";
-		final String DB_PORT = "3306";
-		final String DB_NAME = "nhis";
-		final String DB_URL = 
-				"jdbc:mariadb://" + DB_IP + ":" + DB_PORT + "/" + DB_NAME;
-		
 		try {
 			Class.forName("org.mariadb.jdbc.Driver");
 		}
 		catch(ClassNotFoundException e){
 			e.printStackTrace();
 		}
-		
-		Connection conn = null;	
-		Statement stmt = null;
-		
-		try {
-			//connect DB
-			conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306","root","password");
-			System.out.println("Connection Success!");
+		//connect DB(나중에 알아서 받아)
+		conn = DriverManager.getConnection("jdbc:mysql://localhost:3306","root","1111");
+		System.out.println("Connection Success!");
 			
-			//create sql statements
-			stmt = conn.createStatement();
-			stmt.executeUpdate("use nhis");
+		//create sql statements
+		stmt = conn.createStatement();
+		stmt.executeUpdate("use NHIS");
 			
-			Map<String, Object> nhis_10000 = get_table(stmt, "nhis_10000");
-			Map<String, Object> r1 = get_table(stmt, "r1");
-			r1.forEach((key, value) -> System.out.println(key + ":" + value));
-//			Map<String, Object> original = new HashMap<String, Object>();
-//			
-//			//resultsets = get the result of sql queries
-//			ResultSet rs = stmt.executeQuery("select * from nhis_10000");
-//			ResultSetMetaData rsmd = rs.getMetaData();
-//			int columnCnt = rsmd.getColumnCount();
-//			while(rs.next()) {
-//				for (int i = 1; i <= columnCnt; i++) {
-//					original.put(rsmd.getColumnName(i), rs.getString(rsmd.getColumnName(i)));
-//				}
-////				System.out.println(rs.getString(1)+"\t"+rs.getInt(2)+"\t"+rs.getString(3)+"\t"
-////			+rs.getString(4)+"\t"+rs.getInt(5));
-//			}
-//			original.forEach((key, value) -> System.out.println(key + ":" + value));
-		}
-		catch(SQLException e) {
-			e.printStackTrace();
-		}
+		//resultsets = get the result of sql queries
 
+		HashMap<Integer,ArrayList<HashMap<String,String>>> o_datas = new HashMap<Integer,ArrayList<HashMap<String,String>>>();
+		HashMap<Integer,ArrayList<HashMap<String,String>>> r_datas = new HashMap<Integer,ArrayList<HashMap<String,String>>>();
+			
+		o_datas = get_data("NHIS_10000");
+		r_datas = get_data("R1");
+
+		for(Integer i : o_datas.keySet()) {
+			System.out.println("Key: "+i+" Values: "+o_datas.get(i));
+		}
+		for(Integer i : r_datas.keySet()) {
+			System.out.println("Key: "+i+" Values: "+r_datas.get(i));
+		}
+	}
+	public static HashMap<Integer,ArrayList<HashMap<String,String>>> get_data(String table) throws SQLException {
+		int property_num = 0;
+		String sql1 = "select count(*) from information_schema.columns where table_name = ";
+		StringBuffer sb = new StringBuffer(table);
+		sb.insert(0, "'");
+		sb.insert(sb.length(), "'");
+		
+		rs = stmt.executeQuery(sql1+sb.toString());
+		while(rs.next()) {
+			property_num = rs.getInt(1);
+		}
+		
+		ArrayList<String> properties = new ArrayList<String>();
+		
+		String sql = "desc ";
+		
+		String keyy = "";
+		
+		rs = stmt.executeQuery(sql+table);
+		
+		while(rs.next()) {
+			properties.add(rs.getString(1));
+			if(rs.getString(4).equals("PRI")) {
+				keyy = rs.getString(1);
+			}
+		}
+		keyy = keyy.toLowerCase();
+		System.out.println(keyy);
+		
+		HashMap<Integer,ArrayList<HashMap<String,String>>> Map = new HashMap<Integer,ArrayList<HashMap<String,String>>>();
+		
+		String sql2 = "select * from ";
+		rs = stmt.executeQuery(sql2 + table);
+		
+		ArrayList<HashMap<String, String>> values;
+		HashMap<String,String> p;
+		int count = 1;
+		String s;
+		while(rs.next()) {
+			values = new ArrayList<HashMap<String,String>>();
+			for(int i=1;i<=property_num;i++) {
+				p = new HashMap<String,String>();
+				s = properties.get(i-1).toLowerCase();
+				p.put(s,rs.getString(i));
+				values.add(p);
+				if(s.equals(keyy)) {
+					count = Integer.parseInt(rs.getString(i));
+				}
+			}
+			Map.put(count, values);
+		}
+		return Map;
 	}
 }
